3e36d981f4830c465c3c5c8a1665f902ac79e06c
victoriaacuk <110519614+victoriaacuk@users.noreply.github.com>
2022-08-03 18:11:08 -0700

    Update dataThree.js

--- src/dataThree.js
+++ src/dataThree.js
@@ -17,7 +17,7 @@ export const dataThree = [
         date:"2017",
         status:"Finished",
         description:"As the proud daughter of the Monique family, Aristia was raised to become the next empress of the Castina Empire. But everything collapsed with the appearance of one mysterious girl: the emperor turned away from her, and the unhappy life of Aristia was interrupted by death. However, there was a chance to change her fate, she was reborn as a ten-year-old child! Is this a dream or reality? Will she change her destiny?",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/63/360_o.jpg"
+        image:"https://pbs.twimg.com/media/EdcPE8aUMAEJ6zb.jpg"
     },
 
     {
@@ -27,7 +27,7 @@ export const dataThree = [
         date:"2017",
         status:"Finished",
         description:"In her first life, she was a terrible queen who brought trouble to the country and was burned at the stake by her own king. In the second, she became a talented surgeon, redeeming her guilt for the past by saving people. But due to a plane crash, talented surgeon Song Ji Hyun has returned to her first life! This time, she decided to change everything by becoming a doctor.",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/15/721_o.png"
+        image:"https://i.imgur.com/QW1cZkF.jpeg"
     },
 
     {
@@ -37,7 +37,7 @@ export const dataThree = [
         date:"2018",
         status:"Finished",
         description:"After an unexpected death, Park Eun Ha finds herself in a magical world where everything goes according to the plot of the novel familiar to her. However, she is not the main character of this story, she got a minor role with a quick tragic end! To avoid her unenviable fate, she enters into a contract with Duke Noah Volsteo Winnight. But it's not so simple... Behind the duke's kind and angelic face is a real devil! Can she escape the fate once written in the pages?",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/15/801_o.jpg"
+        image:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1580101654l/45888556._SX318_.jpg"
     },
 
     {
@@ -47,7 +47,7 @@ export const dataThree = [
         date:"2013",
         status:"Finished",
         description:"Oriental music, intricate ornaments, fairy-tale princes, beautiful dancers... It is this atmosphere that envelops the reader in this manhwa. A group of musicians travel from city to city, making a living performing. Her heart is a beautiful harpist. However, it attracted the attention of not only connoisseurs of music ... A prince lives in the palace, who is closer to danger and the sound of a sword than politics and palace intrigues. One day he escapes from his chambers and on his way he meets a gang of robbers...",
-        image:"https://staticrm.rmr.rocks/uploads/pics/00/42/938_o.jpg"
+        image:"https://www.nautiljon.com/images/manga/00/83/sword_of_the_falcon_15538.jpg"
     },
 
     {
@@ -57,7 +57,7 @@ export const dataThree = [
         date:"2019",
         status:"Ongoing",
         description:"The Empress of the Eastern Kingdom was meanly betrayed by her husband, the Emperor, who asked for a divorce in order to marry a random slave girl. But what was the surprise of everyone when, seemingly abandoned, the empress with a smirk asked permission to remarry - with the heir of a neighboring state.",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/58/945_o.jpg"
+        image:"https://cdn.mangaupdates.com/image/i366895.jpg"
     },
 
     {
@@ -67,7 +67,7 @@ export const dataThree = [
         date:"2018",
         status:"Ongoing",
         description:"Canaria is reincarnated as the villian of a popular cliche novel. Based on the novel, she is destined to be executed but can she prevent this from happening before it’s too late?",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/19/051_o.jpg"
+        image:"https://i.pinimg.com/originals/bc/c4/4d/bcc44da2e55246e32207660d0e5f0803.jpg"
     },
 
     {
@@ -77,6 +77,6 @@ export const dataThree = [
         date:"2018",
         status:"Ongoing",
         description:"'Pink Eyes' was able to lead a relatively normal life, except that, firstly, she remembered her past life, and secondly, she was the illegitimate daughter of the empire's only duke. One fine day, when the main character was 11 years old, her prostitute mother decided to show her to her father and demand money for her maintenance. Our only duke instead just bought 'Pink Eyes' for 20,000 gold. How will her life change now? What's next for her?",
-        image:"https://staticrm.rmr.rocks/uploads/pics/01/79/194_o.jpg"
+        image:"https://mngdoom.net/pictures/366/1569765059.jpg"
     }
 ]
